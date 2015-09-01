e6da737a6a24f6c833afe90edcd822e8269567d4
Eudes <aeudes@gmail.com>
2015-09-01 21:16:42 +0000

    Integrativo CBR

---
+++ src/main/java/br/ufpe/cin/integrativocbr/GryphonResultUtil.java
@@ -0,0 +1,82 @@
+package br.ufpe.cin.integrativocbr;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import jcolibri.exception.OntologyAccessException;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import br.ufpe.cin.integrativocbr.GryphonResult;
+
+public class GryphonResultUtil {
+	
+	private static final File defaultJsonFile; 
+	
+	static {
+		defaultJsonFile = new File(GryphonResultUtil.class.getResource("/results").getFile(), 
+				"db_localhost_3306_uniprot.json");
+	}
+	
+	public static String readFile(File resultFile) throws IOException {
+		StringBuilder result = new StringBuilder();
+		BufferedReader reader = new BufferedReader(new FileReader(resultFile));
+		try {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				result.append(line);
+				result.append("\n");
+			}
+			return result.toString();
+		} finally {
+			reader.close();
+		}
+	}
+	
+	public static String removeParenthesesAndOrganismPrefix(String label) {
+		return label.replaceAll("\\(.*?\\)$", "")
+				.replaceFirst("organism #", "")
+				.replaceFirst("biological_process #", "")
+				.replaceFirst("cellular_component #", "");
+	}
+	
+	public static Set<GryphonResult> readResults(File resultFile) throws JSONException, IOException, OntologyAccessException {
+		JSONObject jsonObj = new JSONObject(readFile(resultFile));
+		JSONObject results = jsonObj.getJSONObject("results");
+		JSONArray bindings = results.getJSONArray("bindings");
+		Set<GryphonResult> resultList = new HashSet<>();
+
+		System.out.println("List of Cases:");
+		for (int i = 0; i < bindings.length(); i++) {
+			JSONObject binding = bindings.getJSONObject(i);
+			
+			String[] names = JSONObject.getNames(binding);
+			String label1 = binding.getJSONObject(names[0]).getString("value");
+			String label2 = null;
+			if (names.length > 1) {
+				label2 = binding.getJSONObject(names[1]).getString("value");
+			}
+			
+			GryphonResult result = new GryphonResult();
+			result.setP1(removeParenthesesAndOrganismPrefix(label1));
+			result.setP2(removeParenthesesAndOrganismPrefix(label2));
+			resultList.add(result);
+			
+			System.out.println(result);
+				
+		}
+		System.out.println("Number of Cases from Gryphon: " + resultList.size());
+		return resultList;
+	}
+	
+	public static Set<GryphonResult> readResults() throws JSONException, IOException, OntologyAccessException {
+		return readResults(defaultJsonFile);
+	}
+
+}
---
+++ src/main/java/br/ufpe/cin/integrativocbr/IntegrativoCBRApplication.java
@@ -0,0 +1,189 @@
+package br.ufpe.cin.integrativocbr;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import jcolibri.casebase.LinealCaseBase;
+import jcolibri.cbraplications.StandardCBRApplication;
+import jcolibri.cbrcore.Attribute;
+import jcolibri.cbrcore.CBRCaseBase;
+import jcolibri.cbrcore.CBRQuery;
+import jcolibri.cbrcore.Connector;
+import jcolibri.exception.ExecutionException;
+import jcolibri.exception.OntologyAccessException;
+import jcolibri.method.retrieve.RetrievalResult;
+import jcolibri.method.retrieve.NNretrieval.NNConfig;
+import jcolibri.method.retrieve.NNretrieval.NNScoringMethod;
+import jcolibri.method.retrieve.NNretrieval.similarity.global.Average;
+import jcolibri.method.retrieve.NNretrieval.similarity.local.MaxString;
+import jcolibri.method.retrieve.selection.SelectCases;
+import jcolibri.util.OntoBridgeSingleton;
+
+import org.json.JSONException;
+
+import br.ufpe.cin.integrativocbr.CaseDescription;
+import br.ufpe.cin.integrativocbr.GryphonConnector;
+import br.ufpe.cin.integrativocbr.GryphonResult;
+import br.ufpe.cin.integrativocbr.GryphonResultUtil;
+import br.ufpe.cin.integrativocbr.SimilarityResult;
+import es.ucm.fdi.gaia.ontobridge.OntoBridge;
+import es.ucm.fdi.gaia.ontobridge.OntologyDocument;
+
+public class IntegrativoCBRApplication implements StandardCBRApplication {
+
+	private static Connector connector;
+	
+	private CBRCaseBase caseBase;
+	private Map<String, SimilarityResult> similarityResult;
+	
+	private static Double evalResult;
+	private static String classResult;
+	
+	@Override
+	public void configure() throws ExecutionException {
+		similarityResult = new HashMap<String, SimilarityResult>();
+	}
+
+	@Override
+	public CBRCaseBase preCycle() throws ExecutionException {
+		caseBase = new LinealCaseBase();
+		caseBase.init(connector);
+		
+//		for (CBRCase c : caseBase.getCases()) {
+//			System.out.println(c);
+//		}
+		return caseBase;
+	}
+
+	@Override
+	public void cycle(CBRQuery query) throws ExecutionException {
+		NNConfig simConfig = new NNConfig();
+		simConfig.setDescriptionSimFunction(new Average());
+		simConfig.addMapping(new Attribute("label", CaseDescription.class), new MaxString());
+		
+		Collection<RetrievalResult> results = NNScoringMethod.evaluateSimilarity(caseBase.getCases(), query, simConfig);
+		if (results.isEmpty()) {
+			System.out.println("No results for: " + query);
+		} else {
+			RetrievalResult result = SelectCases.selectTopKRR(results, 1).iterator().next();
+			CaseDescription caseDescription = (CaseDescription) result.get_case().getDescription();
+			evalResult = result.getEval();
+			classResult = caseDescription.getClassId();
+		}
+	}
+
+	@Override
+	public void postCycle() throws ExecutionException {
+	}
+
+	private static OntoBridge configureOntoBridge() {
+		String basePath = IntegrativoCBRApplication.class.getResource("/mscExperiment").getFile() + File.separator;
+		String baseSourcesPath = basePath + "sources/";
+		
+		OntoBridge ob = OntoBridgeSingleton.getOntoBridge();
+		ob.initWithOutReasoner();
+		
+		OntologyDocument mainOnto = new OntologyDocument(basePath + "TesteGryphon.owl");
+		List<OntologyDocument> subOntologies = new ArrayList<>();
+		subOntologies.add(new OntologyDocument(baseSourcesPath + "btl2.owl"));
+		subOntologies.add(new OntologyDocument(baseSourcesPath + "chebi_homocysteine.owl"));
+		subOntologies.add(new OntologyDocument(baseSourcesPath + "DiseaseList.owl"));
+		subOntologies.add(new OntologyDocument(baseSourcesPath + "go_module.owl"));
+		subOntologies.add(new OntologyDocument(baseSourcesPath + "ncbitaxon.owl"));
+		subOntologies.add(new OntologyDocument(baseSourcesPath + "PR_protein.owl"));
+		
+		ob.loadOntology(mainOnto, subOntologies, true);
+		return ob;
+	}
+	
+	public Map<String, SimilarityResult> getSimilarityResult() {
+		return similarityResult;
+	}
+	
+	
+	public static void main(String[] args) throws OntologyAccessException, ExecutionException, JSONException, IOException {
+		configureOntoBridge();
+		
+		IntegrativoCBRApplication app = new IntegrativoCBRApplication();
+		app.configure();
+		
+		
+//		Q1
+//		===
+//		FIXME old code
+//		System.out.println(">>>> MONOMOLECULARENTITY TREE");
+//		doCycle(app, "http://purl.org/biotop/btl2.owl#MonoMolecularEntity");
+//		System.out.println(">>>> ORGANISM TREE");
+//		doCycle(app, "http://purl.org/biotop/btl2.owl#Organism");
+		
+		// Q2
+		// ===
+		System.out.println(">>>> GO_0008150 TREE");
+		GryphonConnector connectorA = new GryphonConnector("http://purl.obolibrary.org/obo/GO_0008150");
+		System.out.println(">>>> ORGANISM TREE");
+		GryphonConnector connectorB = new GryphonConnector("http://purl.org/biotop/btl2.owl#Organism");
+		
+		// Q3
+		// ===
+//		System.out.println(">>>> GO_0008150 TREE");
+//		GryphonConnector connectorA = new GryphonConnector("http://purl.obolibrary.org/obo/GO_0008150");
+//		System.out.println(">>>> GO_0005575 TREE");
+//		GryphonConnector connectorB = new GryphonConnector("http://purl.obolibrary.org/obo/GO_0005575");
+		
+		// Q4
+		// ===
+//		System.out.println(">>>> GO_0008150 TREE");
+//		GryphonConnector connectorA = new GryphonConnector("http://purl.obolibrary.org/obo/GO_0008150");
+//		System.out.println(">>>> PR_000000001 TREE");
+//		GryphonConnector connectorB = new GryphonConnector("http://purl.obolibrary.org/obo/PR_000000001");
+		
+		CBRQuery query;
+		
+		for (GryphonResult gryphonResult : GryphonResultUtil.readResults()) {
+			// A
+			// ===
+			connector = connectorA;
+			
+			query = new CBRQuery();
+			query.setDescription(new CaseDescription(null, gryphonResult.getP1()));
+			
+			app.preCycle();
+			app.cycle(query);
+			app.postCycle();
+			
+			double evalResultA = evalResult;
+			String classResultA = classResult;
+			
+			// B
+			// ===
+			connector = connectorB;
+			
+			query = new CBRQuery();
+			query.setDescription(new CaseDescription(null, gryphonResult.getP2()));
+			
+			app.preCycle();
+			app.cycle(query);
+			app.postCycle();
+			
+			double evalResultB = evalResult;
+			String classResultB = classResult;
+
+			// Average
+			// =======
+			
+			System.out.println(">> " + gryphonResult);
+			System.out.println("   >> classA = " + classResultA);
+			System.out.println("   >> evalA = " + evalResultA);
+			System.out.println("   >> classB = " + classResultB);
+			System.out.println("   >> evalB = " + evalResultB);
+			System.out.println("   >> average= " + (evalResultA + evalResultB) / 2);
+			
+		}
+		
+	}
+}
