67bba993b1a592df119433a42bcf3bf61aa60f1a
Eudes <aeudes@gmail.com>
2015-10-27 06:44:36 -0300

    configs ok

---
+++ cin.ufpe.integrativocbr.core/libs/installscript.sh
@@ -0,0 +1,2 @@
+mvn install:install-file -Dfile=jcolibri2.jar -DartifactId=jcolibri -DgroupId=jcolibri -Dversion=2 -DgeneratePom=true -Dpackaging=jar
+mvn install:install-file -Dfile=ontobridge.jar -DartifactId=ontobridge -DgroupId=ontobridge -Dversion=latest -DgeneratePom=true -Dpackaging=jar
