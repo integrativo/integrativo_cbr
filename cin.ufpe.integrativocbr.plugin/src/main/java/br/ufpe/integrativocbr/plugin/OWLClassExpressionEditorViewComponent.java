35c06687b1b74271e754f76f13c8bbce66a99272
Eudes <aeudes@gmail.com>
2015-10-27 06:44:36 -0300

    No comment.

--- cin.ufpe.integrativocbr.plugin/src/main/java/br/ufpe/integrativocbr/plugin/OWLClassExpressionEditorViewComponent.java
+++ cin.ufpe.integrativocbr.plugin/src/main/java/br/ufpe/integrativocbr/plugin/OWLClassExpressionEditorViewComponent.java
@@ -3,21 +3,34 @@ package br.ufpe.integrativocbr.plugin;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 
+import org.aksw.owl2sparql.OWLClassExpressionToSPARQLConverter;
 import org.protege.editor.owl.ui.clsdescriptioneditor.ExpressionEditor;
 import org.protege.editor.owl.ui.clsdescriptioneditor.OWLExpressionChecker;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
 import org.semanticweb.owlapi.model.OWLClassExpression;
+import org.semanticweb.owlapi.model.OWLException;
+
+import br.ufpe.cin.aac3.gryphon.Gryphon;
+import br.ufpe.cin.aac3.gryphon.GryphonConfig;
 
 public class OWLClassExpressionEditorViewComponent extends AbstractOWLViewComponent {
 
 	private static final long serialVersionUID = 1L;
 
+	private ExpressionEditor<OWLClassExpression> expressionEditor;
+	
 	@Override
 	protected void initialiseOWLView() throws Exception {
 		setLayout(new BorderLayout(10, 10));
@@ -33,11 +46,56 @@ public class OWLClassExpressionEditorViewComponent extends AbstractOWLViewCompon
 		resultsPanel.add(new JLabel("Results"));
 		return resultsPanel;
 	}
+	
+	private JPanel createButtonsPanel() {
+		JButton testSparqlConversionButton = new JButton();
+		testSparqlConversionButton.setText("Test Sparql Conversion");
+		testSparqlConversionButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				testSparqlConversionButtonAction();
+			}
+		});
+		
+		JButton testGryphonButton = new JButton();
+		testGryphonButton.setText("Teste Gryphon");
+		testGryphonButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				testGryphonButtonAction();
+			}
+		});
 
+		JPanel buttonsPanel = new JPanel();
+		buttonsPanel.setLayout(new FlowLayout());
+		buttonsPanel.add(testGryphonButton);
+		return buttonsPanel;
+	}
+	
+	private void testGryphonButtonAction() {
+		System.out.println("*** INIT Test Gryphon");
+		GryphonConfig.setWorkingDirectory(new File("integrationExample"));
+		GryphonConfig.setLogEnabled(true);
+		GryphonConfig.setShowLogo(true);
+		Gryphon.init();
+		System.out.println("*** END Test Gryphon");
+	}
+
+	private void testSparqlConversionButtonAction() {
+		OWLClassExpressionToSPARQLConverter converter = new OWLClassExpressionToSPARQLConverter();
+		String text;
+		try {
+			text = converter.convert(expressionEditor.createObject(), "?x", false);
+			JOptionPane.showMessageDialog(this, "SPARQL conversion:\n" + text);
+		} catch (OWLException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	private JPanel createQueryPanel() {
 		JPanel editorPanel = new JPanel(new BorderLayout());
 		OWLExpressionChecker<OWLClassExpression> checker = getOWLModelManager().getOWLExpressionCheckerFactory().getOWLClassExpressionChecker();
-		ExpressionEditor<OWLClassExpression> expressionEditor = new ExpressionEditor<OWLClassExpression>(getOWLEditorKit(), checker);
+		expressionEditor = new ExpressionEditor<OWLClassExpression>(getOWLEditorKit(), checker);
 
 		editorPanel.setBorder(BorderFactory.createCompoundBorder(BorderFactory.createTitledBorder(BorderFactory.createLineBorder(
                 Color.LIGHT_GRAY), "DL Query (class expression)"), BorderFactory.createEmptyBorder(3, 3, 3, 3)));
@@ -45,6 +103,7 @@ public class OWLClassExpressionEditorViewComponent extends AbstractOWLViewCompon
 		expressionEditor.setPreferredSize(new Dimension(100, 50));
 		
 		editorPanel.add(expressionEditor, BorderLayout.CENTER);
+		editorPanel.add(createButtonsPanel(), BorderLayout.SOUTH);
 		return editorPanel;
 	}
 
